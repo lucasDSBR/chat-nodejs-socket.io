48863d5c78ca25d58ba9a8d0d1277b05f15ef864
lucasDSBR <lucasmaciel6690@gmail.com>
2024-06-23 11:03:32 -0300

    update send message init project

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+io.on("connection", (socket) => {
+
+	//Identificando desconexao
+	socket.on("disconnect", () => {
+		console.log("X desconectou: " + socket.id);
+	});
+
+});
+
+app.set("view engine", "ejs");
+
+
+app.get('/', (req, res) => {
+	res.render("index")
+});
+
+
+http.listen(3000, () => {
+	console.log("app running...")
+});
